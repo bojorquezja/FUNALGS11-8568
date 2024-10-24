67a6d8a832c029d580b7b898726df1c1e5db913f
Fabri-Programmer <fabrizziopks@example.com>
2024-10-24 10:39:39 -0500

    modi1

--- Ejercicios 11/Program.cs
+++ Ejercicios 11/Program.cs
@@ -2,7 +2,7 @@
 int r = int.Parse(Console.ReadLine());
 int[] valor = new int[r];
 Random ra = new Random();
-int c = 0, c1 = 0, total = 0, total1 = 0, p = 0, im = 0;
+int p = 0, im = 0;
 for (int i = 0; i < valor.Length; i++)
 {
     valor[i] = ra.Next(50, 90);
@@ -32,6 +32,8 @@ else if (h == "impares")
 {
     Console.WriteLine("\nLos numeros impares son = " + im);
 }
+
+Console.Write("el mas duro de la nueva ");
 
 
 
