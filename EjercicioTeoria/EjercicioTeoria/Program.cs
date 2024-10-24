fa85bc6d44ca334ca372d49296d612094e6483c2
Wifth <alexander.obando.os2206@gmail.com>
2024-10-24 10:50:00 -0500

    Avance 3

--- EjercicioTeoria/EjercicioTeoria/Program.cs
+++ EjercicioTeoria/EjercicioTeoria/Program.cs
@@ -1,32 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int c = 0;
-Random r = new Random();
+int c = 0, x = 0;
 
 Console.Write("Ingrese cantidad de elementos: ");
 int n = int.Parse(Console.ReadLine());
+
+int[] numeros = new int[n];
+Random ran = new Random();
+
+for (x = 0; x < n; x++)
+{
+    numeros[x] = ran.Next(10, 99 + 1);
+}
+
+if (x % 2 == 0)
+{
+    c =+ 0;
+}
+else if (x % 2 != 0)
+{
+    c =+ 0;
+}
+
+string todos = String.Join(", ", numeros);
+Console.WriteLine($"Números: {todos}");
 
 Console.Write("¿Prefiere los números pares (P) o impares (I)?: ");
 string v = Console.ReadLine().ToUpper();
 Console.WriteLine();
 
-while (c < n)
-{
-    
-    int numero = r.Next(50, 89 + 1);
-
-    if (v == "P" && numero % 2 == 0)
-    {
-        Console.Write($"{ numero} ");
-        c++;
-    }
-    else if (v == "I" && numero % 2 != 0)
-    {
-        Console.WriteLine($"{numero} ");
-        c++;
-    }
-}
-Console.WriteLine();
 if (v == "P")
 {
     Console.WriteLine("Cantidad de pares: " + c);
