f0760aa9f4f69cf91c4d991a470f28864f1467a7
Wifth <alexander.obando.os2206@gmail.com>
2024-10-24 11:06:32 -0500

    Avance 4

--- EjercicioTeoria/EjercicioTeoria/Program.cs
+++ EjercicioTeoria/EjercicioTeoria/Program.cs
@@ -1,39 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int c = 0, x = 0;
+using System.Threading.Channels;
+
+int ci = 0, cp = 0, x;
 
 Console.Write("Ingrese cantidad de elementos: ");
 int n = int.Parse(Console.ReadLine());
+Console.Write("¿Prefiere los números pares (P) o impares (I)?: ");
+string v = Console.ReadLine().ToUpper();
+Console.WriteLine();
 
 int[] numeros = new int[n];
 Random ran = new Random();
 
 for (x = 0; x < n; x++)
 {
-    numeros[x] = ran.Next(10, 99 + 1);
-}
+    numeros[x] = ran.Next(50, 89 + 1);
 
-if (x % 2 == 0)
-{
-    c =+ 0;
-}
-else if (x % 2 != 0)
-{
-    c =+ 0;
+    if (numeros[x] % 2 == 0)
+    {
+        cp++;
+    }
+    else if (numeros[x] % 2 != 0)
+    {
+        ci++;
+    }      
 }
 
 string todos = String.Join(", ", numeros);
 Console.WriteLine($"Números: {todos}");
-
-Console.Write("¿Prefiere los números pares (P) o impares (I)?: ");
-string v = Console.ReadLine().ToUpper();
 Console.WriteLine();
-
 if (v == "P")
 {
-    Console.WriteLine("Cantidad de pares: " + c);
+    Console.WriteLine("Cantidad de pares: " + cp);
 }
 else if (v == "I")
 {
-    Console.WriteLine("Cantidad de impares: " + c);
+    Console.WriteLine("Cantidad de impares: " + ci);
 }
