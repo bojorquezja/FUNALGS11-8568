c46fbaf01050bde973e1f8f4cf6c48a63a2a3a8d
Fabri-Programmer <fabrizziopks@example.com>
2024-10-24 10:16:59 -0500

    Cambio de nuevo

--- Ejercicios 11/Program.cs
+++ Ejercicios 11/Program.cs
@@ -2,22 +2,35 @@
 int r = int.Parse(Console.ReadLine());
 int[] valor = new int[r];
 Random ra = new Random();
-int c = 0, c1 = 0, total = 0, total1 = 0;
+int c = 0, c1 = 0, total = 0, total1 = 0, p = 0, im = 0;
 for (int i = 0; i < valor.Length; i++)
 {
-    total = valor[i] = ra.Next(50, 90);
+    valor[i] = ra.Next(50, 90);
+
+
+    if (valor[i] % 2 == 0)
+        {
+            p++;
+        }
+        else
+        {
+            im++;
+        }
+
     
+
 }
 Console.Write("¿Te gustan los pares o impares? = ");
 string h = Console.ReadLine();
 if (h == "pares")
 {
-    if (total % 2 == 0)
-    {
-        c++;
-    }
+    Console.WriteLine("pares = " + p);
     
 }
-Console.WriteLine("valores pares = " + total);
+if (h == "impares")
+{
+    Console.WriteLine("impares = " + im);
+}
+
 
 
