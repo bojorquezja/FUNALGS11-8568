002ed896e9481c905d058f2ef2aaca71cf6dcce4
bojorquezja <bojorquez.ja@gmail.com>
2024-10-31 10:42:22 -0500

    arreglos

---
+++ insertavalor/Program.cs
@@ -0,0 +1,28 @@
+﻿void imprime(int[] lis) {
+    foreach (int x in lis) {
+        Console.WriteLine(x);
+    }
+}
+
+int[] lista = {5,6,8,9};
+
+imprime(lista);
+
+/*
+ * a-pedir numero y posicion
+ * b-aumentar tamaño de arreglo
+ * c-desplazar numeros hacia el final hasta insertar nuevo valor
+*/
+
+Console.WriteLine("Ingrese numero a insertar");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese posicion donde insertar");
+int p = Convert.ToInt32(Console.ReadLine()) - 1;
+
+Array.Resize(ref lista, lista.Length + 1);
+for (int x = lista.Length-1; x > p; x--) {  //x = x-1
+    lista[x] = lista[x - 1];
+}
+lista[p] = n;
+
+imprime(lista);
---
+++ semana12/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+void imprime(int[] lis) {
+    foreach (int x in lis) {
+        Console.WriteLine(x);
+    }
+}
+
+int[] lista = new int[5];
+
+Random rand = new Random();
+for (int x = 0; x < lista.Length; x++) {
+    lista[x] = rand.Next(1, 10 + 1);
+}
+imprime(lista);
+
+Console.WriteLine();
+Console.WriteLine("Ingrese nuevo numero");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Array.Resize(ref lista, lista.Length + 1);
+lista[lista.Length - 1] = n;
+imprime(lista);
